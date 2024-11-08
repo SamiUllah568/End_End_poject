2d72cbabb45c77be9f9358286b8f149722fe142f
SamiUllah568 <sk2579784@gmail.com>
2024-11-08 14:56:52 +0500

    artifacts/proprocessor.pkl

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,10 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 
 @dataclass
 class DataIngestionConfig:
@@ -20,17 +24,18 @@ class DataIngestion:
         self.Ingestion_config = DataIngestionConfig()
 
 
-    def initiate_data_Ingestion(self):
+    def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
             data = pd.read_csv('D:\\DataScienceProject\\end_to_end_project\\notebook\\data\\StudentsPerformance.csv')
             logging.info('read the dataset as datafame')
+            
             os.makedirs(os.path.dirname(self.Ingestion_config.train_data_path) , exist_ok=True)
 
             data.to_csv(self.Ingestion_config.raw_data_path , index = False , header = True)
 
             logging.info("Train test split initiated")
-            train_set ,test_set =train_test_split(data ,train_size=0.2 , random_state=42)
+            train_set ,test_set =train_test_split(data ,test_size=0.2 , random_state=42)
 
             train_set.to_csv(self.Ingestion_config.train_data_path ,index =False , header = True)
 
@@ -42,11 +47,12 @@ class DataIngestion:
                 self.Ingestion_config.train_data_path,
                 self.Ingestion_config.test_data_path)
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(sys,e)
         
 
 if __name__== "__main__":
     obj=DataIngestion()
-    obj.initiate_data_Ingestion()
-
+    train_data , test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data , test_data)
 
